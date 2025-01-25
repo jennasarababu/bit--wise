6fa882699341626e7f10ef362be0e234fd8950d0
jennasarababu <132893476+jennasarababu@users.noreply.github.com>
2025-01-26 05:22:44 +0530

    Delete sb directory

--- sb/daily_notes/notes/forms.py
+++
@@ -1,8 +0,0 @@
-# # forms.py
-# from django import forms
-# from .models import Photo
-
-# class PhotoUploadForm(forms.ModelForm):
-#     class Meta:
-#         model = Photo
-#         # fields = ['title', 'image']  # Specify the fields you want in the 
